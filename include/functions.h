c18a254adbcdc2f3dc9c9145a0c8b258367a7e3f
quincinia <61718793+quincinia@users.noreply.github.com>
2020-03-02 18:59:56 -0800

    Update functions.h

--- include/functions.h
+++ include/functions.h
@@ -4,15 +4,19 @@
 using namespace std;
 
 
-
+//does what it says
 void printBoard(Piece board[][8]);
 
+//returns 1 if there are valid moves for that piece
 bool turn(char color, Piece board[][8]);
 
+//gets coordinates from user, updates board if those coordinates correspond to a valid move
 void place(char color, Piece board[][8]);
 
+//removes all valid move markers
 void clearPairs(Piece board[][8]);
 
+//returns 0 for tie, 1 for white win, 2 for black win
 int outcome(const int white, const int black);
 
 #endif
